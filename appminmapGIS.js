d7a6664bcc1de5e6fdf92631e0c36355b0d94f87
kopairkehidupan <ang.budgetcontrol@aeknatio.co.id>
2025-12-02 11:36:21 +0700

    Update appminmapGIS.js

--- appminmapGIS.js
+++ appminmapGIS.js
@@ -1675,7 +1675,7 @@ async function exportPdfFromLayers() {
                         const labelTextColor = hexToRgb(meta.labelSettings.textColor || '#000000');
                         
                         // ===== UKURAN FONT LEBIH KECIL =====
-                        const labelSize = 6;  // 
+                        const labelSize = 7;  // 
                         
                         // Hitung ukuran polygon di layar (pixel)
                         const polyBounds = turf.bbox(f);
